f06e65c72c3ae09d0adfe7567855c30c69891a48
Vadim <duninv@gmail.com>
2020-04-27 09:10:00 +0300

    step_1

---
+++ testmainpage.py
@@ -0,0 +1,9 @@
+from .mainpage import MainPage
+
+# pytest -v --tb=line --language=en test_main_page.py
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
